4dd67ec8a6c7c0a7d457441086a2eba31008a0d1
demon-cpu <rddojka@gmail.com>
2024-09-15 08:08:31 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -251,7 +251,8 @@ async def thumbrm(client, message):
     data = user_data[user_id]
     await DbManger().update_user_data(user_id)
     await DbManger().update_user_doc(user_id, 'thumb')
-    await sendMessage(message, "Thumbnail Removed")
+    msg = await message.reply_text("Thumbnail Removed")
+    await message.delete()
   
 async def main():
     await gather(start_cleanup(), torrent_search.initiate_search_tools(), restart_notification(), search_images(), set_commands(bot), log_check())
